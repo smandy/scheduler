7ddcefcfa0fc254ec90032a6492cb0092d521579
Andy Smith <andyrsmith@googlemail.com>
2016-12-16 15:05:22 +0000

    Add the gem server.

--- java/core/src/main/java/bamma/Runner.java
+++ java/core/src/main/java/bamma/Runner.java
@@ -7,13 +7,29 @@ import Ice.Util;
 
 public class Runner {
     public static void main(String[] args) {
+        System.out.println("Create communicator");
         Communicator communicator = Util.initialize(args);
-
         Properties props = communicator.getProperties();
+
         String adapterName = props.getProperty("adapterName");
         String objectIdentity = props.getProperty("objectIdentity");
+
+        System.out.println("objectIdentity = " + objectIdentity);
+        System.out.println("adapterName = " + adapterName);
+
         ObjectAdapter adapter = communicator.createObjectAdapter(adapterName);
         GemServer server = new GemServer();
+
+        System.out.println("server = " + server);
         adapter.add(server, communicator.stringToIdentity(objectIdentity));
+        adapter.activate();
+
+        System.out.println("Waiting for communicator shutdown");
+        communicator.waitForShutdown();
+
+        System.out.println("Destroying");
+
+        communicator.destroy();
+        System.out.println("Destroyed - exiting");
     }
 }
